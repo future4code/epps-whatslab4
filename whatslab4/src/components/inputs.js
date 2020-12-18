d6224dd5c5d33157cd1583d85165fe7d0af8e320
Sandro-Tome <sandrotome.jr@gmail.com>
2020-12-18 12:24:26 -0300

    Criação lógica para os inputs

--- whatslab4/src/components/inputs.js
+++ whatslab4/src/components/inputs.js
@@ -1,14 +1,60 @@
 import React from "react"
 
-
 class Inputs extends React.Component{
+
+  state = {
+    mensagem: [{
+
+    }],
+    valorInputUsuario: '',
+    valorInputMensagem: ''
+  }
+
+  adicionaMensagem = () => {
+    const novaMensagem = {
+      usuario: this.state.valorInputUsuario,
+      mensagem: this.state.valorInputMensagem
+    }
+
+    const novoMensagem = [
+      ...this.setState.mensagem, novaMensagem
+    ];
+    this.setState({ mensagem: novoMensagem})
+  };
+
+  onChangeinputUsuario = (event) => {
+    this.setState({valorInputUsuario: event.target.value})
+  }
+
+  onChangeinputMensagem = (event) => {
+    this.setState({valorInputMensagem: event.target.value})
+  }
+
   render(){
+    const listaMensagens = this.state.mensagem.map((novaMensagem) => {
+      return (
+        <p>
+          {novaMensagem.usuario}
+        </p>
+      )
+    })
+
     return(
       <div>
-        <input placeholder={"Nome"}/>
-        <input placeholder={'Mensagem'} />
-        <button>Enviar mensagem</button>
-      
+        <div>
+          <input 
+            placeholder={"Nome"}
+            value={this.state.valorInputUsuario}
+            onChange={this.state.onChangeinputUsuario}
+          />
+          <input 
+            placeholder={'Mensagem'}
+            value={this.state.valorInputMensagem}
+            onChange={this.state.onChangeinputMensagem} 
+          />
+          <button onClick={this.adicionaMensagem}>Enviar mensagem</button>
+        </div>
+        <div>{listaMensagens}</div>
       </div>
     )
   }
